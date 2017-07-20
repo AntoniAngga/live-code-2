741ea23d087c61328e37b854526d2c31ab67042b
Antoni Angga <antoniangga14@gmail.com>
2017-07-20 14:08:12 +0700

    Token keluar juga T.T

--- controller/cLogin.js
+++ controller/cLogin.js
@@ -1,12 +1,24 @@
 const User = require('../model/Users');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 let Login = (req,res) =>{
   let data = req.body;
   User.find({ username: data.username }, (err, user) => {
     if (user) {
-      console.log(user)
+      if (user.length > 0 ) {
+        let password_decode = bcrypt.compareSync(data.password, user[0].password);
+        if (password_decode) {
+          let token = jwt.sign({ name: user[0].name}, 'secret');
+          res.status(200).send(token);
+        } else {
+          res.status(503).send('Sorry Password Wrong');
+        }
+      } else {
+        res.status(503).send('Sorry Username Wrong');
+      }
     } else {
-      console.log(err)
+      res.status(501).send('Something wrong with your login',err);
     }
   });
 } 
--- routes/user_login.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var cLogin = require('../controller/cLogin');
-/* GET login API. */
-router.get('/', cLogin.Login);
-
-module.exports = router;
